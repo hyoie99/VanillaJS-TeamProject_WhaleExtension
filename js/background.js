e05dbf21d7758cbc082d33d38362a34a49ed7452
hyen <80624327+hyoie99@users.noreply.github.com>
2022-09-08 21:09:19 +0900

    modify 'newtab' in 'manifest'

--- js/background.js
+++ js/background.js
@@ -1,5 +1,12 @@
 chrome.runtime.onInstalled.addListener(async () => {
   let url = chrome.runtime.getURL("../html/main.html");
-  let tab = await chrome.tabs.create({ url });
-  console.log(`Created tab ${tab.id}`);
+  chrome.tabs.create({ url });
 });
+
+// chrome.tabs.onCreated.addListener(async () => {
+//   let url = chrome.runtime.getURL("../html/main.html");
+//   let tab = await chrome.tabs.create({ url });
+//   console.log(`Created tab ${tab.id}`);
+// });
+
+// chrome.tabs.onCreated.addListener(function () {});
