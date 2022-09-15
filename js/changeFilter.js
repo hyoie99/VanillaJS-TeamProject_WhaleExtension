ef1e270dbec871b57338d23f7f3a15d09449d2ac
hyen <80624327+hyoie99@users.noreply.github.com>
2022-09-15 11:21:34 +0900

    fix error

--- js/changeFilter.js
+++ js/changeFilter.js
@@ -1,21 +1,42 @@
+if (localStorage.getItem("filter") == null) {
+  localStorage.setItem("filter", "encourage");
+}
+
 const btns = document.querySelectorAll(".filter-btn");
 
 function selectBtn(event) {
-  const targetBtn = event.path[1];
+  const targetBtn = event.composedPath(1);
+  // const targetImg = targetBtn.querySelector("img");
+  const targetImg = targetBtn[0];
+  const targetImg2 = targetBtn[0].nextElementSibling;
+  // console.log(targetImg);
+  // console.log(targetImg2);
+  // console.log(targetBtn[1]);
 
   localStorage.removeItem("today0");
   localStorage.removeItem("today1");
   localStorage.removeItem("today2");
+  localStorage.removeItem("extraContent");
 
   btns.forEach((btn) => {
     btn.id = "";
+    btn.querySelectorAll("img")[0].classList = "";
+    btn.querySelectorAll("img")[1].classList = "hidden";
   });
-  targetBtn.id = "selected";
+  targetBtn[1].id = "selected";
+  // targetImg.classList.toggle = "hidden";
+  targetImg.classList = "hidden";
+  targetImg2.classList = "";
+  // targetImg[0].classList = "hidden";
+  // targetImg[1].classList = "";
+
   saveFilter(targetBtn);
 }
 
 function saveFilter(btn) {
-  const filter = btn.querySelector("button").classList[0];
+  const filter = btn[1].querySelector("button").classList;
+  // const filter = btn[1].classList;
+  // console.log(btn[1]);
   localStorage.setItem("filter", filter);
 }
 
@@ -25,12 +46,16 @@ function getFilter() {
     btn.id = "";
     if (btn.querySelector("button").classList[0] == savedFilter) {
       btn.id = "selected";
+      btn.querySelectorAll("img")[0].classList = "hidden";
+      btn.querySelectorAll("img")[1].classList = "";
     }
   });
 }
 
 btns.forEach((btn) => {
-  btn.addEventListener("click", selectBtn);
+  console.log(btn.querySelector("img"));
+  btn.querySelector("img").addEventListener("click", selectBtn);
+  // btn.addEventListener("click", selectBtn);
 });
 
 getFilter();
